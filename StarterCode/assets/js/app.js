8c9cd8593c997bc6b637ba5be40f9774f031083f
blizzardfun <38262823+blizzardfun@users.noreply.github.com>
2018-12-10 16:04:19 -0600

    labels added working on swithc axis

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -44,9 +44,9 @@ function makeResponsive() {
   
     var margin = {
       top: 50,
-      bottom: 50,
-      right: 70,
-      left: 50
+      bottom: 90,
+      right: 50,
+      left: 60
     };
   
     var height = svgHeight - margin.top - margin.bottom;
@@ -72,6 +72,38 @@ function yScale(surveyData, chosenYAxis){
     return YLinearScale;
     }
 
+// function used for updating xAxis var upon click on axis label
+function xRenderAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+  
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
+  
+    return xAxis;
+  }
+
+  // function used for updating yAxis var upon click on axis label
+function yRenderAxes(newYScale, yAxis) {
+    var leftAxis = d3.axisLeft(newYScale);
+  
+    yAxis.transition()
+      .duration(1000)
+      .call(leftAxis);
+  
+    return yAxis;
+  }
+// function used to update circles on a change in x or y axis
+function renderCircles (circlesGroup, xScale, yScale, chosenXAxis, chosenYAxis ){
+    circlesGroup.selectAll("g")
+        .transition()
+        .duration(1000)
+        .attr("transform",
+        d=> `translate( ${xScale(d[chosenXAxis])}, 
+                   ${yScale(d[chosenYAxis])})`);
+    return circlesGroup;
+}
+  
 //***************************************************** */
 
     // Append SVG element
@@ -114,22 +146,110 @@ function yScale(surveyData, chosenYAxis){
   //to contain circles and text
   var circlesGroup = chartGroup.selectAll("circle")
         .data(surveyData)
-    var element = circlesGroup.enter()
+    var elemEnter = circlesGroup.enter()
         .append("g")
         .attr("transform",
-            d=> `translate( ${xLinearScale(d.obesity)}, 
-                       ${yLinearScale(d.poverty)})`)
+            d=> `translate( ${xLinearScale(d[chosenXAxis])}, 
+                       ${yLinearScale(d[chosenYAxis])})`)
 
-        var circle= element.append("circle")
+        /* circle for each block */
+        var newcircle= elemEnter.append("circle")
             .attr("r", 20)
             .classed("stateCircle" ,true)
             .attr("opacity", ".5");
 
-        var text = element.append("text")
+        /* Create the text for each block */
+        elemEnter.append("text")
             .attr("text-anchor", "middle")
-            .attr("dy",5)
-            // .classed("stateText chart" ,true)
-            .attr("text", d=> d.abbr);
+            .attr("dy", 5)
+            .text(d => d.abbr);
+
+  // Create group for  2 x- axis labels **************
+  var xlabelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+  var obesityLabel = xlabelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("value", "obesity") // value to grab for event listener
+    .classed("active", true)
+    .text("Percent of Adults aged 18 and older who have Obesity");
+
+  var smokesLabel = xlabelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 40)
+    .attr("value", "smokes") // value to grab for event listener
+    .classed("inactive", true)
+    .text("Percentage of Adults aged 18 and older who Smoke");
+
+// Create group for  2 y- axis labels **************
+var ylabelsGroup = chartGroup.append("g")
+.attr("transform", `translate(0, ${height/2}) rotate (-90)`);
+
+var povertyLabel = ylabelsGroup.append("text")
+.attr("x", 0)
+.attr("y", -30)
+.attr("value", "poverty") // value to grab for event listener
+.classed("active", true)
+.text("Percent of Households Below the poverty Line");
+
+var incomeLabel = ylabelsGroup.append("text")
+.attr("x", 0)
+.attr("y", -50)
+.attr("value", "income") // value to grab for event listener
+.classed("inactive", true)
+.text("Average Income");
+
+/****************************************************** */
+/* event listeners */
+// x axis labels event listener
+xlabelsGroup.selectAll("text")
+.on("click", function() {
+  // get value of selection
+  var value = d3.select(this).attr("value");
+  if (value !== chosenXAxis) {
+
+    // replaces chosenXAxis with value
+    chosenXAxis = value;
+
+    console.log(chosenXAxis)
+
+    // updates x scale for new data
+    xLinearScale = xScale(surveyData, chosenXAxis);
+
+    // updates x axis with transition
+    xAxis = xRenderAxes(xLinearScale, xAxis);
+
+    // updates circles with new x values
+    circlesGroup = renderCircles(circlesGroup, xLinearScale, yLinearScale, chosenXAxis, chosenYAxis);
+
+    // updates tooltips with new info
+    // circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+    // changes classes to change bold text
+    // if (chosenXAxis === "num_albums") {
+    //   albumsLabel
+    //     .classed("active", true)
+    //     .classed("inactive", false);
+    //   hairLengthLabel
+    //     .classed("active", false)
+    //     .classed("inactive", true);
+    // }
+    // else {
+    //   albumsLabel
+    //     .classed("active", false)
+    //     .classed("inactive", true);
+    //   hairLengthLabel
+    //     .classed("active", true)
+    //     .classed("inactive", false);
+    // }
+  }
+});
+
+
+
+
+
 
 
   };  //function makeResponsive
